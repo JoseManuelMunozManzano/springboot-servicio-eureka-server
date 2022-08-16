ae299bd6d2be22a6e6e900da04e4d359271caba0
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-16 17:14:06 +0200

    Habilitamos Eureka Server

--- src/main/java/com/jmunoz/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ src/main/java/com/jmunoz/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -2,7 +2,10 @@ package com.jmunoz.springboot.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// Habilitamos Eureka Server
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringbootServicioEurekaServerApplication {
 
